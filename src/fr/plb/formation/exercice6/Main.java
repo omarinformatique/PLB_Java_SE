0fc7470a80faf6cb00828a1f1092f19206194def
sauvageb <sauvageboris.pro@gmail.com>
2023-11-14 10:08:34 +0100

    Exercice 5 et 6 oop students

--- src/fr/plb/formation/exercice6/Main.java
+++ src/fr/plb/formation/exercice6/Main.java
@@ -1,6 +1,5 @@
-package fr.plb.formation.exercice5;
+package fr.plb.formation.exercice6;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,31 +9,37 @@ public class Main {
     public static void main(String[] args) {
         // Initialise le scanner pour la saisie clavier
         Scanner scanner = new Scanner(System.in);
-        String[] firstNames = new String[NB_STUDENTS];
-        Float[] marks = new Float[NB_STUDENTS];
+
+        Student[] students = new Student[NB_STUDENTS];
 
         System.out.println("Démarrage de la Saisie des notes par élèves :");
         for (int i = 0; ; i++) {
+
             System.out.println("Prénom de l'élève :");
             String firstName = scanner.next();
             if (firstName.equals("-1")) {
                 System.out.println("Arrêt de la saisie.");
                 break;
             }
+
+            System.out.println("Nom de l'élève :");
+            String lastName = scanner.next();
+
+            System.out.println("Age de l'élève :");
+            int age = scanner.nextInt();
+
             System.out.println("Note de l'élève :");
-            Float mark = scanner.nextFloat();
-            if (mark == -1) {
-                System.out.println("Arrêt de la saisie.");
-                break;
-            }
-            firstNames[i] = firstName;
-            marks[i] = mark;
+            float mark = scanner.nextFloat();
+
+            students[i] = new Student(firstName, lastName, mark, age);
         }
 
         // Affichage de la saisie
+
         for (int i = 0; i < NB_STUDENTS; i++) {
-            if (firstNames[i] != null) {
-                System.out.println(firstNames[i] + " --> " + marks[i]);
+            Student currentStudent = students[i];
+            if (currentStudent != null) {
+                currentStudent.display();
             }
         }
         scanner.close();
---
+++ src/fr/plb/formation/exercice6/Student.java
@@ -0,0 +1,23 @@
+package fr.plb.formation.exercice6;
+
+public class Student {
+
+    // Attributs
+    String firstname;
+    String lastname;
+    float mark;
+    int age;
+
+    // Constructeur
+    public Student(String firstname, String lastname, float mark, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.mark = mark;
+        this.age = age;
+    }
+
+
+    public void display() {
+        System.out.println(this.firstname + " " + this.lastname + " -->" + this.mark);
+    }
+}
